3408dc0b8c7341c5da30477a7784671f9c0c8881
MiltonMatias22 <nodout.develops@gmail.com>
2018-11-22 10:33:19 -0200

    Use DB::beginTransaction() to balance & historic

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use DB;
 class Balance extends Model
 {
     public $timestamps = false;
@@ -24,6 +24,9 @@ class Balance extends Model
 
     public function deposit(float $vl) : Array
     {
+
+        DB::beginTransaction();
+
         $totalBefore = $this->amount ? $this->amount : 0;
 
         $this->amount += number_format($vl, 2, '.', ',');
@@ -38,15 +41,23 @@ class Balance extends Model
         ]);
 
         if ($dep && $hist) {
-           return [
-               'success' => true,
-               'message' => 'Deposit successful'
-           ];
-        }
+            
+            DB::commit();
+           
+            return [
+                'success' => true,
+                'message' => 'Deposit successful'
+            ];
 
-        return [
-            'success' => false,
-            'message' => 'Deposit failed'
-        ];
+        }else {          
+
+            DB::rollback();
+
+            return [
+                'success' => false,
+                'message' => 'Deposit failed'
+            ];
+
+        }
     }
 }
